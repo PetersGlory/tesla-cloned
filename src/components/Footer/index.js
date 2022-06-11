308fdac2669f20941bc9120d9c2a24ea8dc64ea8
Peter <59800244+PetersGlory@users.noreply.github.com>
2022-06-11 08:04:50 -0700

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,12 +8,16 @@ const Footer = ({bottom}) =>{
         <Wrapper>
             <Content className={bottom} >
                 <div>
-                <a href="https://github.com/chirag-23/Tesla-clone-reactjs" target='_blank' >Tesla-Clone &copy; 2022</a>
+                <a href="https://github.com/PetersGlory/tesla-john" target='_blank' >Tesla-Clone &copy; 2022</a>
                 </div>
                 <div>
-                <a href="mailto:chiragchouhan163@gmail.com" target='_blank'>Gmail</a>
-                <a href="https://www.linkedin.com/in/chirag-chouhan-b48311228/" target="_blank" >LinkedIn</a>
-                <a href="https://github.com/chirag-23" target="_blank" >GitHub</a>
+                <a href="#" target='_blank'>Privacy & Legal</a>
+                <a href="#" target='_blank'>Vehicle Recalls</a>
+                <a href="#" target='_blank'>Contact</a>
+                <a href="#" target='_blank'>Careers</a>
+                <a href="#" target="_blank" >News</a>
+                <a href="#" target="_blank" >Engage</a>
+                <a href="#" target="_blank" >Locations</a>
                 </div>
             </Content>
         </Wrapper>
